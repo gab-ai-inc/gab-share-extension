f0b1b6470c8115580deadd32eafb3ff7bc48e90e
mgabdev <dev.mi.ma.rk22@gmail.com>
2019-02-17 15:11:03 -0500

    Added twitter content script, tweet+gab functionality
    Added twitter content script, tweet+gab functionality
    
    • Added:
    - Twitter content script in manifest
    - Twitter compile in gulpfile function: scripts
    - Twitter content script in src/scripts

--- gulpfile.js
+++ gulpfile.js
@@ -229,6 +229,7 @@ gulp.task('scripts', () => {
         promises.push(getPromise('options', 'src/scripts/components/options.js', browser));
         promises.push(getPromise('background', 'src/scripts/background/**', browser));
         promises.push(getPromise('script', 'src/scripts/content/sharegab/**', browser, 'content/sharegab'));
+        promises.push(getPromise('script', 'src/scripts/content/twitter/**', browser, 'content/twitter'));
     };
 
     return Promise.all(promises);
